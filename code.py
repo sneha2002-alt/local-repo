a572dceeb2691ca1bd941425172606b4ffa1f720
Python codes <snehamittal.sm22@gmail.com>
2026-03-30 22:01:36 +0530

    indexing

--- code.py
+++ code.py
@@ -3,6 +3,10 @@ print("Happy Learning")
 
 str1="hii"
 str2="hello"
-final_str = str1 + str2
+final_str = str1 + str2 #concatenation
 print(final_str)
 print(len(final_str))
+#indexing
+str= "Happy_Learning"
+ch=str[0]
+print(ch)

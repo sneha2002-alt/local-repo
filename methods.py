cead1686cbaae8bea9f3f0dc9655375457d090d4
Python codes <snehamittal.sm22@gmail.com>
2026-04-09 21:34:36 +0530

    commit

--- methods.py
+++ methods.py
@@ -5,14 +5,14 @@ class Laptop:
     self.RAM = RAM
     self.storage = storage
     
-  @classmethod 
+  @classmethod  # Class method
   def get_storage_type(cls):
     print(f"storage type = {cls.storage_type}")  
     
-  def get_info(self):
+  def get_info(self):   # Instance method
     print(f"Laptop has {self.RAM} RAM and {self.storage} storage")  
     
-  @staticmethod
+  @staticmethod  # Static Method
   def calc_discount(price, discount):
     final_price = price - (discount*price/100)
     print(f"discounted price = {final_price}")
@@ -23,3 +23,4 @@ l2 = Laptop("8GB", "256GB")
 l1.get_info()
 l1.get_storage_type()
 l1.calc_discount(40_000, 10)
+Laptop.get_storage_type()

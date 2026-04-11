ab51f51e8d21e359e4013f2f6f085bf03e847202
Python codes <snehamittal.sm22@gmail.com>
2026-04-11 17:08:06 +0530

    commit

---
+++ check_word.py
@@ -0,0 +1,25 @@
+def check_word():
+    with open("practice.txt", "r") as f:
+        word = "learning"
+        data = f.read()
+        if(data.find(word) != -1):
+            print("Found")
+        else:
+            print("Not found")  
+
+def check_line():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if (word in data):
+                print(line_no)
+                return
+            line_no += 1
+            
+    return -1        
+
+check_word()  
+check_line()
